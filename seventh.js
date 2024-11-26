91ee880275bc36b373f398f96ce7893f783acc07
keerusai <141726571+keerusai@users.noreply.github.com>
2024-11-26 20:04:40 +0530

    Add files via upload

---
+++ seventh.js
@@ -0,0 +1,10 @@
+/*Array.array.forEach(callbackfunction){  
+});
+callback function:A callback function passed as an argument to another function*/
+//it checks for each value in an array
+
+let arr=[1,2,3,4,5];
+    arr.forEach((val)=>
+    {
+        console.log(val)
+    })
