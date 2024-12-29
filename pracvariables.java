5b7f75cfe1e9d6591b56984202b655f44becfc83
keerusai <141726571+keerusai@users.noreply.github.com>
2024-12-29 18:32:04 +0530

    Add files via upload

---
+++ pracvariables.java
@@ -0,0 +1,12 @@
+//to caluculate: a*b/a-b where as a=10 , b=5
+
+public class pracvariables {
+    
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=5;
+        int cal=(a*b)/(a-b);
+        System.out.println(cal);
+    }
+}
