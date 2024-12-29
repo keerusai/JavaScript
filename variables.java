2f471893f8cdde3685c84c6d4c640233b5158555
keerusai <141726571+keerusai@users.noreply.github.com>
2024-12-29 18:18:05 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args)
+    {
+        //variables
+        int a =10;
+        int b =20;
+        
+        int mul=a*b;
+        System.out.println(mul);
+    }
+}
